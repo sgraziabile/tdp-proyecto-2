8cda17cdc5e5161eab345fda163d9fd928f3e927
Julian Gallardo <juliangallardo26603@gmail.com>
2022-10-09 14:06:02 -0300

    Arreglo error .project

---
+++ src/Logica/Visitor.java
@@ -0,0 +1,26 @@
+package Logica;
+import Entidades.*;
+
+public interface Visitor {
+	
+	public void visitarPared(Pared p);
+	
+	public void visitarFrutilla(Frutilla f);
+	
+	public void visitarUva(Uva u);
+	
+	public void visitarSandia(Sandia s);
+	
+	public void visitarManzana(Manzana m);
+	
+	public void visitarBanana(Banana b);
+	
+	public void visitarVerde(Verde v);
+	
+	public void visitarAzul(Azul a);
+	
+	public void visitarRojo(Roja r);
+	
+	public void visitarCuerpo(Pared p);
+	
+}
